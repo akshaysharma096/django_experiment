cfac3e77eb8fd16aa2bf335c7fd69a05dd3ef221
Somin <sominwadhwa@gmail.com>
2017-11-13 17:52:36 +0530

    Added forms & security

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
 	# For http://127.0.0.1:8000/post/1/, we have -- 
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'), 
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
 
 ''' 
