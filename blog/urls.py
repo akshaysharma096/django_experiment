b2d788b200ad60b13a2b5d9305d737671450491f
Somin <sominwadhwa@gmail.com>
2017-11-13 17:15:05 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,16 @@ Assign a view called 'post_list' to the url ^$
 '''
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
+	# For http://127.0.0.1:8000/post/1/, we have -- 
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'), 
 ]
+
+''' 
+(?P<pk>\d+) – this part is trickier. It means that Django will take
+everything that you place here and transfer it to a view as a variable called
+pk. (Note that this matches the name we gave the primary key variable back in
+blog/templates/blog/post_list.html!) \d also tells us that it can only be a
+digit, not a letter (so everything between 0 and 9). + means that there needs
+to be one or more digits there. So something like http://127.0.0.1:8000/post//
+is not valid, but http://127.0.0.1:8000/post/1234567890/ is perfectly OK! 
+'''
